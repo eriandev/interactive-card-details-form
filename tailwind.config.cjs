760df07bd3d044dd5122f7806e465d81a59176b7
Erick Vargas <erianvc.dev@gmail.com>
2023-01-18 22:35:09 -0500

    feat: adds style guide & resources

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,20 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      'space-grotesk': ['Space Grotesk', 'Arial', 'Noto Sans', 'sans-serif'],
+    },
+
+    colors: {
+      white: 'hsl(0, 0%, 100%)',
+      error: 'hsl(0, 100%, 66%)',
+      violet: 'hsl(278, 94%, 30%)',
+      'very-dark-violet': 'hsl(278, 68%, 11%)',
+      'very-light-violet': 'hsl(249, 99%, 64%)',
+      'dark-grayish-violet': 'hsl(279, 6%, 55%)',
+      'light-grayish-violet': 'hsl(270, 3%, 87%)',
+      transparent: 'transparent',
+    },
   },
   plugins: [],
 };
